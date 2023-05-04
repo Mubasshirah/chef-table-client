bdca21cc26aaf21bc1dd6e96ea7860689d23be98
Mubasshirah <adibaju1994@gmail.com>
2023-05-04 21:08:32 +0600

    environment keyhide

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCXyef62OCjMSOSXFjnOBCpqvVhBmEwxrE",
-  authDomain: "assignment-10-chef-table.firebaseapp.com",
-  projectId: "assignment-10-chef-table",
-  storageBucket: "assignment-10-chef-table.appspot.com",
-  messagingSenderId: "420438715254",
-  appId: "1:420438715254:web:054e5d8d4c1282569a6857"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
 };
 
 // Initialize Firebase
